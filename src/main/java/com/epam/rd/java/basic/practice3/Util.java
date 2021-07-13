8a3c10bdf08ade9413eb27a7d05115974f7b3dd0
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2021-07-13 21:08:21 +0300

    Part final 2

--- src/main/java/com/epam/rd/java/basic/practice3/Util.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Util.java
@@ -5,11 +5,11 @@ package com.epam.rd.java.basic.practice3;
         import java.io.File;
         import java.io.IOException;
         import java.util.Scanner;
+        import java.util.logging.*;
 
 public class Util {
-    private static BootstrapLogger logger;
-
     public static String getInput(String fileName) {
+        Logger logger = Logger.getAnonymousLogger();
         StringBuilder sb = new StringBuilder();
         try {
             Scanner scanner = new Scanner(new File(fileName), "UTF-8");
@@ -19,7 +19,7 @@ public class Util {
             scanner.close();
             return sb.toString().trim();
         } catch (IOException ex) {
-            logger.log(ex);
+            logger.log(Level.SEVERE, "IOException");
         }
         return sb.toString();
     }
